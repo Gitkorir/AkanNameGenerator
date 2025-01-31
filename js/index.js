99f97745be87a8ab56af6510198012c1298af906
arnold korir <arnoldkorir201@gmail.com>
2025-01-31 14:31:52 +0300

    craeted the js file structure and called an annonymous function

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('akanForm');
+    const result = document.getElementById('result');
+    const error = document.getElementById('error');
+    const akanNameElement = document.getElementById('akanName');
+}
