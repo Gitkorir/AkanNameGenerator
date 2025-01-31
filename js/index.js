5326020203296ff9fcff1bfa630d8ad411f28d74
arnold korir <arnoldkorir201@gmail.com>
2025-01-31 14:34:55 +0300

    created an Akan- Male Names Array

--- js/index.js
+++ js/index.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const result = document.getElementById('result');
     const error = document.getElementById('error');
     const akanNameElement = document.getElementById('akanName');
+
+     const AKAN_NAMES = {
+        male: {
+            0: 'Kwasi',    // Sunday
+            1: 'Kwadwo',   // Monday
+            2: 'Kwabena',  // Tuesday
+            3: 'Kwaku',    // Wednesday
+            4: 'Yaw',      // Thursday
+            5: 'Kofi',     // Friday
+            6: 'Kwame'     // Saturday
+        },
 }
